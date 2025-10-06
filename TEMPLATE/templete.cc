828e67b7bb620fac0980e05f60f9d31f0c582948
callrajveer <rehanshugupta1234@gmail.com>
2025-10-06 11:05:11 +0530

    added

---
+++ TEMPLATE/templete.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX INT_MAX
+#define MIN INT_MIN
+#define ll long long int
+#define nline "\n"
+#define BIG 998244353
+#define MOD 1000000007
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pii pair<ll,ll>
+#define vi vector<ll>
+#define vb vector<bool>
+#define vc vector<char>
+#define vs vector<string>
+#define vvi vector<vi>
+#define vpii vector<pii>
+#define ff first
+#define ss second
+#define mkp make_pair
+#define pb emplace_back
+
+
+void solve(){
+    
+}
+int main()
+{
+    fast_io;
+    
+    int t=1;
+    cin>>t;
+    while(t--) 
+        solve();
+}

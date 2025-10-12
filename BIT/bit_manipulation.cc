f8ba3fbaec7d7920ff04cff11d31a9177c27f904
callrajveer <rehanshugupta1234@gmail.com>
2025-10-12 20:24:05 +0530

    added

--- BIT/bit_manipulation.cc
+++ BIT/bit_manipulation.cc
@@ -16,5 +16,22 @@ int main(){
     cout<<(n<<1)<<endl; //left shift by 1 is basically multiply by 2
 
     //character uppercase to lowercase and vice versa
+    //binary representation of A and a is differ only on 5th position
+    for(char i='A'; i<='Z'; i++){
+        cout<<i<<endl;
+        printBinary(i);
+    }
     
+    for(char i='a'; i<='z'; i++){
+        cout<<i<<endl;
+        printBinary(i);
+    }
+    char A='A';
+    //to get A to a set 1 to binary rep of A at position 5 basically
+    char a = A | (1<<5);
+    cout<<a<<endl;
+    //
+
+
+
 }

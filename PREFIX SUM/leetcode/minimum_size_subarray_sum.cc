2a1bb78a576b3b6e30606092470e63aa2b2f1493
callrajveer <rehanshugupta1234@gmail.com>
2025-10-17 14:40:00 +0530

    added

--- PREFIX SUM/leetcode/minimum_size_subarray_sum.cc
+++ PREFIX SUM/leetcode/minimum_size_subarray_sum.cc
@@ -10,9 +10,10 @@ using namespace std;
 
 */
 
+//560. Subarray Sum Equals K
 class Solution {
 public:
-    int minSubArrayLen(int target, vector<int>& nums) {
+    int subarraySum(int target, vector<int>& nums) {
         int n = nums.size();
         int cnt=0;
         vector<int> prefixSum(n,0);
@@ -27,10 +28,42 @@ public:
         for(int j=0; j<n; j++){
             if(prefixSum[j]==target) cnt++;
             
+            int val = prefixSum[j]-target;
+
+            //check agar val map me exist krta hai toh uski freq cnt me add krdo
+            if(m.find(val) != m.end()){
+                cnt += m[val];
+            }
+            
+            //check agr prefixsum ka jo values hai usko map me dalkr freq increase krdo
+            if(m.find(prefixSum[j]) == m.end()){
+                m[prefixSum[j]]=0;
+            }
+            m[prefixSum[j]]++;
         }
-        
+        return cnt;
     }
 };
 
-vector<int> a={2,3,1,2,4,3};
-int target = 7;
+//209. Minimum Size Subarray Sum
+
+class Solution{
+public:
+    int minSubArrayLen(int target , vector<int>& nums){
+       int n = nums.size();
+       vector<int> prefixSum(n,0);
+       prefixSum[0]=nums[0];
+       for(int i=1; i<n; i++){
+        prefixSum[i]=prefixSum[i-1]+nums[i];
+       }
+       int left=0;
+       int miniLen=INT_MAX;
+       for(int right=0; right<n; right++){
+        while(prefixSum[right]-(left>0 ? prefixSum[left-1]:0) >= target){
+            miniLen=min(miniLen,right-left +1);
+            left++;
+        }
+       }
+       return (miniLen==INT_MAX ? 0 : miniLen);
+    }
+};
